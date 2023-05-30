69c6dbe87a6b3b05cd3cb47a34deb5d6a333f3ca
Arthur <47847427+77wiz77@users.noreply.github.com>
2023-05-30 21:50:24 +1100

    fix
    off autoslide
    add meta

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,57 +1,57 @@
-"use strict"
+'use strict';
 const prev = document.getElementsByClassName('slider__btn-prev')[0],
-      next = document.getElementsByClassName('slider__btn-next')[0],
-      slides = document.querySelectorAll('.slider__slide'),
-      dots = document.querySelectorAll('.slider__dot');
+  next = document.getElementsByClassName('slider__btn-next')[0],
+  slides = document.querySelectorAll('.slider__slide'),
+  dots = document.querySelectorAll('.slider__dot');
 
 let index = 0;
 
-const activeSlide = n => {
-  for(let slide of slides) {
+const activeSlide = (n) => {
+  for (let slide of slides) {
     slide.classList.remove('active');
   }
   slides[n].classList.add('active');
-}
+};
 
-const activeDot = n => {
-  for(let dot of dots) {
+const activeDot = (n) => {
+  for (let dot of dots) {
     dot.classList.remove('active');
   }
   dots[n].classList.add('active');
-}
+};
 
-const prepareCurrentSlide = ind => {
+const prepareCurrentSlide = (ind) => {
   activeSlide(index);
   activeDot(index);
-}
+};
 
 const nextSlide = () => {
-  if(index == slides.length - 1) {
+  if (index == slides.length - 1) {
     index = 0;
     prepareCurrentSlide(index);
-  }else {
+  } else {
     index++;
     prepareCurrentSlide(index);
   }
-}
+};
 
 const prevSlide = () => {
-  if(index == 0) {
+  if (index == 0) {
     index = slides.length - 1;
     prepareCurrentSlide(index);
-  }else {
+  } else {
     index--;
     prepareCurrentSlide(index);
   }
-}
+};
 
-dots.forEach((item, indexDot)=> {
+dots.forEach((item, indexDot) => {
   item.addEventListener('click', () => {
     index = indexDot;
     prepareCurrentSlide(index);
-  })
-})
+  });
+});
 
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
-setInterval( nextSlide, 5000 );
+// setInterval( nextSlide, 5000 );
